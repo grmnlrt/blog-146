a75fa6c1e5604183fbd1f80d62e99054c53e3c84
Germain Loret <germain.loret@gmail.com>
2018-05-18 10:29:51 +0200

    ready for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_071741) do
+ActiveRecord::Schema.define(version: 2018_05_18_082010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_05_18_071741) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
 end
